95cec2b7d4a7ae284c311a3f066d2ae2fece2309
Oscar Marin <oscar@marin.cr>
2022-01-06 23:27:51 -0600

    improvements

---
+++ src/Luval.DataStore.xTests/When_A_Sql_Expression_Is_Used.cs
@@ -0,0 +1,52 @@
+﻿using Luval.DataStore.DataAnnotations;
+using Luval.DataStore.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Luval.DataStore.xTests
+{
+    public class When_A_Sql_Expression_Is_Used
+    {
+        [Fact]
+        public void It_Should_Generate_A_Valid_Where_Statement_With_String_Constant_Comparison()
+        {
+            var printer = new SqlExpressionPrinter();
+            Expression<Func<Entity, bool>> expression = (i => i.Name == "Oscar");
+            var sql = printer.Where(expression);
+            Assert.True(sql == "(Name = 'Oscar')");
+        }
+
+        [Fact]
+        public void It_Should_Generate_A_Valid_Where_Statement_With_String_Multiple_Constant_Comparison()
+        {
+            var printer = new SqlExpressionPrinter();
+            Expression<Func<Entity, bool>> expression = (i => i.Name == "Oscar" && i.Id == "25" || i.Id == "30");
+            var sql = printer.Where(expression);
+            Assert.True(sql == "(((Name = 'Oscar') AND (Id = '25')) OR (Id = '30'))");
+        }
+
+        [Fact]
+        public void It_Should_Generate_A_Valid_Where_Statement_With_Annotated_Column()
+        {
+            var printer = new SqlExpressionPrinter();
+            Expression<Func<Entity, bool>> expression = (i => i.Salary == 15d);
+            var sql = printer.Where(expression);
+            Assert.True(sql == "(SalaryColumn = 15)");
+        }
+    }
+
+    public class Entity
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime AgeDate { get; set; }
+        [ColumnName("SalaryColumn")]
+        public double? Salary { get; set; }
+    }
+}
---
+++ src/Luval.DataStore/Extensions/MemberInfoExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Luval.DataStore.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luval.DataStore.Extensions
+{
+    public static class MemberInfoExtensions
+    {
+        /// <summary>
+        /// Gets the annotated name of the member
+        /// </summary>
+        /// <param name="info">The <see cref="MemberInfo"/> to use</param>
+        /// <returns>The <see cref="MemberInfo"/> annoted name</returns>
+        public static string GetName(this MemberInfo info)
+        {
+            if (info.CustomAttributes == null || !info.CustomAttributes.Any()) return info.Name;
+            var ca = info.GetCustomAttributes().FirstOrDefault(i => i.GetType() == typeof(ColumnNameAttribute));
+            if (ca != null) return ((ColumnNameAttribute)ca).Name;
+            var ta = info.GetCustomAttributes().FirstOrDefault(i => i.GetType() == typeof(TableNameAttribute));
+            if (ca != null) return ((TableNameAttribute)ta).TableName.GetFullTableName();
+            return info.Name;
+        }
+    }
+}

53dfdd768054b6fb4fe53e104282a81b360f067a
Oscar Marin <oscar@marin.cr>
2022-01-08 22:30:46 -0600

    Fixed issues with the extensions

--- src/Luval.DataStore/Extensions/UnitOfWorkExtensions.cs
+++ src/Luval.DataStore/Extensions/UnitOfWorkExtensions.cs
@@ -13,6 +13,7 @@ namespace Luval.DataStore.Extensions
     /// </summary>
     public static class UnitOfWorkExtensions
     {
+
         #region Standard CRUD Operations
 
         /// <summary>
@@ -23,7 +24,7 @@ namespace Luval.DataStore.Extensions
         /// <param name="entity">The entity data to persist</param>
         /// <returns>Number of affected records</returns>
         /// <exception cref="ArgumentNullException"></exception>
-        public static int AddAndSave<TEntity>(IUnitOfWork<TEntity> uow, TEntity entity) where TEntity : class
+        public static int AddAndSave<TEntity>(this IUnitOfWork<TEntity> uow, TEntity entity) where TEntity : class
         {
             if (uow == null) throw new ArgumentNullException(nameof(uow));
             if (entity == null) throw new ArgumentNullException(nameof(entity));
@@ -41,7 +42,7 @@ namespace Luval.DataStore.Extensions
         /// <param name="cancellationToken">Operation cancellation notification token</param>
         /// <returns>A <see cref="Task{TResult}"/> with the affected records</returns>
         /// <exception cref="ArgumentNullException"></exception>
-        public static Task<int> AddAndSaveAsync<TEntity>(IUnitOfWork<TEntity> uow, TEntity entity, CancellationToken cancellationToken) where TEntity : class
+        public static Task<int> AddAndSaveAsync<TEntity>(this IUnitOfWork<TEntity> uow, TEntity entity, CancellationToken cancellationToken) where TEntity : class
         {
             if (uow == null) throw new ArgumentNullException(nameof(uow));
             if (entity == null) throw new ArgumentNullException(nameof(entity));
@@ -58,7 +59,7 @@ namespace Luval.DataStore.Extensions
         /// <param name="entity">The entity data to persist</param>
         /// <returns>Number of affected records</returns>
         /// <exception cref="ArgumentNullException"></exception>
-        public static int UpdateAndSave<TEntity>(IUnitOfWork<TEntity> uow, TEntity entity) where TEntity : class
+        public static int UpdateAndSave<TEntity>(this IUnitOfWork<TEntity> uow, TEntity entity) where TEntity : class
         {
             if (uow == null) throw new ArgumentNullException(nameof(uow));
             if (entity == null) throw new ArgumentNullException(nameof(entity));
@@ -76,7 +77,7 @@ namespace Luval.DataStore.Extensions
         /// <param name="cancellationToken">Operation cancellation notification token</param>
         /// <returns>A <see cref="Task{TResult}"/> with the affected records</returns>
         /// <exception cref="ArgumentNullException"></exception>
-        public static Task<int> UpdateAndSaveAsync<TEntity>(IUnitOfWork<TEntity> uow, TEntity entity, CancellationToken cancellationToken) where TEntity : class
+        public static Task<int> UpdateAndSaveAsync<TEntity>(this IUnitOfWork<TEntity> uow, TEntity entity, CancellationToken cancellationToken) where TEntity : class
         {
             if (uow == null) throw new ArgumentNullException(nameof(uow));
             if (entity == null) throw new ArgumentNullException(nameof(entity));
@@ -93,7 +94,7 @@ namespace Luval.DataStore.Extensions
         /// <param name="entity">The entity data to delete</param>
         /// <returns>Number of affected records</returns>
         /// <exception cref="ArgumentNullException"></exception>
-        public static int DeleteAndSave<TEntity>(IUnitOfWork<TEntity> uow, TEntity entity) where TEntity : class
+        public static int DeleteAndSave<TEntity>(this IUnitOfWork<TEntity> uow, TEntity entity) where TEntity : class
         {
             if (uow == null) throw new ArgumentNullException(nameof(uow));
             if (entity == null) throw new ArgumentNullException(nameof(entity));
@@ -111,7 +112,7 @@ namespace Luval.DataStore.Extensions
         /// <param name="cancellationToken">Operation cancellation notification token</param>
         /// <returns>A <see cref="Task{TResult}"/> with the affected records</returns>
         /// <exception cref="ArgumentNullException"></exception>
-        public static Task<int> DeleteAndSaveAsync<TEntity>(IUnitOfWork<TEntity> uow, TEntity entity, CancellationToken cancellationToken) where TEntity : class
+        public static Task<int> DeleteAndSaveAsync<TEntity>(this IUnitOfWork<TEntity> uow, TEntity entity, CancellationToken cancellationToken) where TEntity : class
         {
             if (uow == null) throw new ArgumentNullException(nameof(uow));
             if (entity == null) throw new ArgumentNullException(nameof(entity));
@@ -124,6 +125,8 @@ namespace Luval.DataStore.Extensions
 
         #region Upsert Operations
 
+        #region Standard Upset
+
         /// <summary>
         /// Adds or updates the entity to the <see cref="IDataEntityCollection{TEntity}"/> and saves to the target <see cref="IDataStore"/>
         /// </summary>
@@ -134,7 +137,7 @@ namespace Luval.DataStore.Extensions
         /// <returns>Number of affected records</returns>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="InvalidOperationException"></exception>
-        public static int AddOrUpdateAndSave<TEntity>(IUnitOfWork<TEntity> uow, TEntity entity, Expression<Func<TEntity, bool>> filterExpression) where TEntity : class
+        public static int AddOrUpdateAndSave<TEntity>(this IUnitOfWork<TEntity> uow, TEntity entity, Expression<Func<TEntity, bool>> filterExpression) where TEntity : class
         {
             if (uow == null) throw new ArgumentNullException(nameof(uow));
             if (entity == null) throw new ArgumentNullException(nameof(entity));
@@ -161,7 +164,7 @@ namespace Luval.DataStore.Extensions
         /// <returns>Number of affected records</returns>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="InvalidOperationException"></exception>
-        public static int AddOrUpdateAndSave<TEntity>(IUnitOfWork<TEntity> uow, IEnumerable<TEntity> entities, Expression<Func<TEntity, bool>> filterExpression) where TEntity : class
+        public static int AddOrUpdateAndSave<TEntity>(this IUnitOfWork<TEntity> uow, IEnumerable<TEntity> entities, Expression<Func<TEntity, bool>> filterExpression) where TEntity : class
         {
             if (uow == null) throw new ArgumentNullException(nameof(uow));
             if (entities == null) throw new ArgumentNullException(nameof(entities));
@@ -194,7 +197,7 @@ namespace Luval.DataStore.Extensions
         /// <returns>A <see cref="Task{TResult}"/> with the affected records</returns>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="InvalidOperationException"></exception>
-        public static async Task<int> AddOrUpdateAndSaveAsync<TEntity>(IUnitOfWork<TEntity> uow, TEntity entity, Expression<Func<TEntity, bool>> filterExpression, CancellationToken cancellationToken) where TEntity : class
+        public static async Task<int> AddOrUpdateAndSaveAsync<TEntity>(this IUnitOfWork<TEntity> uow, TEntity entity, Expression<Func<TEntity, bool>> filterExpression, CancellationToken cancellationToken) where TEntity : class
         {
             if (uow == null) throw new ArgumentNullException(nameof(uow));
             if (entity == null) throw new ArgumentNullException(nameof(entity));
@@ -222,7 +225,7 @@ namespace Luval.DataStore.Extensions
         /// <returns>A <see cref="Task{TResult}"/> with the affected records</returns>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="InvalidOperationException"></exception>
-        public static async Task<int> AddOrUpdateAndSaveAsync<TEntity>(IUnitOfWork<TEntity> uow, IEnumerable<TEntity> entities, Expression<Func<TEntity, bool>> filterExpression, CancellationToken cancellationToken) where TEntity : class
+        public static async Task<int> AddOrUpdateAndSaveAsync<TEntity>(this IUnitOfWork<TEntity> uow, IEnumerable<TEntity> entities, Expression<Func<TEntity, bool>> filterExpression, CancellationToken cancellationToken) where TEntity : class
         {
             if (uow == null) throw new ArgumentNullException(nameof(uow));
             if (entities == null) throw new ArgumentNullException(nameof(entities));
@@ -242,13 +245,13 @@ namespace Luval.DataStore.Extensions
             }
 
             return await uow.SaveChangesAsync(cancellationToken);
-        }
+        } 
+        #endregion
 
         /// <summary>
         /// Adds or updates the audit based entity to the <see cref="IDataEntityCollection{TEntity}"/> and saves to the target <see cref="IDataStore"/>
         /// </summary>
         /// <typeparam name="TEntity">The data entity <see cref="Type"/></typeparam>
-        /// <typeparam name="TKey">The entity Id <see cref="Type"/></typeparam>
         /// <param name="uow">Unit of Work to use</param>
         /// <param name="entity">The entity data to persist</param>
         /// <param name="userId">The user Id that is affecting the entity</param>
@@ -256,7 +259,7 @@ namespace Luval.DataStore.Extensions
         /// <returns>Number of affected records</returns>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="InvalidOperationException"></exception>
-        public static int AddOrUpdateAndSave<TEntity, TKey>(IUnitOfWork<TEntity> uow, TEntity entity, string userId, Expression<Func<TEntity, bool>> filterExpression) where TEntity : class, IAuditedEntity<TKey>
+        public static int AddOrUpdateAndSave<TEntity>(this IUnitOfWork<TEntity> uow, TEntity entity, string userId, Expression<Func<TEntity, bool>> filterExpression) where TEntity : class, IAuditedEntity
         {
             if (uow == null) throw new ArgumentNullException(nameof(uow));
             if (entity == null) throw new ArgumentNullException(nameof(entity));
@@ -265,7 +268,6 @@ namespace Luval.DataStore.Extensions
             {
                 if (entries.Count() > 1) throw new InvalidOperationException(string.Format("{0} contains more than one record", nameof(filterExpression)));
 
-                entity.Id = entries.First().Id;
                 entity.UtcUpdatedOn = DateTime.UtcNow;
                 entity.UpdatedByUserId = userId;
 
@@ -287,7 +289,6 @@ namespace Luval.DataStore.Extensions
         /// Adds or updates the audit based entity to the <see cref="IDataEntityCollection{TEntity}"/> and saves to the target <see cref="IDataStore"/>
         /// </summary>
         /// <typeparam name="TEntity">The data entity <see cref="Type"/></typeparam>
-        /// <typeparam name="TKey">The entity Id <see cref="Type"/></typeparam>
         /// <param name="uow">Unit of Work to use</param>
         /// <param name="entities">The collection of entities to persist</param>
         /// <param name="userId">The user Id that is affecting the entity</param>
@@ -295,7 +296,7 @@ namespace Luval.DataStore.Extensions
         /// <returns>Number of affected records</returns>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="InvalidOperationException"></exception>
-        public static int AddOrUpdateAndSave<TEntity, TKey>(IUnitOfWork<TEntity> uow, IEnumerable<TEntity> entities, string userId, Expression<Func<TEntity, bool>> filterExpression) where TEntity : class, IAuditedEntity<TKey>
+        public static int AddOrUpdateAndSave<TEntity>(this IUnitOfWork<TEntity> uow, IEnumerable<TEntity> entities, string userId, Expression<Func<TEntity, bool>> filterExpression) where TEntity : class, IAuditedEntity
         {
             if (uow == null) throw new ArgumentNullException(nameof(uow));
             if (entities == null) throw new ArgumentNullException(nameof(entities));
@@ -307,7 +308,6 @@ namespace Luval.DataStore.Extensions
                 {
                     if (entries.Count() > 1) throw new InvalidOperationException(string.Format("{0} contains more than one record", nameof(filterExpression)));
 
-                    entity.Id = entries.First().Id;
                     entity.UtcUpdatedOn = DateTime.UtcNow;
                     entity.UpdatedByUserId = userId;
 
@@ -331,7 +331,6 @@ namespace Luval.DataStore.Extensions
         /// Adds or updates the audit based entity to the <see cref="IDataEntityCollection{TEntity}"/> and saves to the target <see cref="IDataStore"/>
         /// </summary>
         /// <typeparam name="TEntity">The data entity <see cref="Type"/></typeparam>
-        /// <typeparam name="TKey">The entity Id <see cref="Type"/></typeparam>
         /// <param name="uow">Unit of Work to use</param>
         /// <param name="entity">The entity data to persist</param>
         /// <param name="filterExpression">Expression used to identify if the record already exists in the <see cref="IDataStore"/></param>
@@ -340,7 +339,7 @@ namespace Luval.DataStore.Extensions
         /// <returns>A <see cref="Task{TResult}"/> with the affected records</returns>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="InvalidOperationException"></exception>
-        public static async Task<int> AddOrUpdateAndSaveAsync<TEntity, TKey>(IUnitOfWork<TEntity> uow, TEntity entity, string userId, Expression<Func<TEntity, bool>> filterExpression, CancellationToken cancellationToken) where TEntity : class, IAuditedEntity<TKey>
+        public static async Task<int> AddOrUpdateAndSaveAsync<TEntity>(this IUnitOfWork<TEntity> uow, TEntity entity, string userId, Expression<Func<TEntity, bool>> filterExpression, CancellationToken cancellationToken) where TEntity : class, IAuditedEntity
         {
             if (uow == null) throw new ArgumentNullException(nameof(uow));
             if (entity == null) throw new ArgumentNullException(nameof(entity));
@@ -349,7 +348,6 @@ namespace Luval.DataStore.Extensions
             {
                 if (entries.Count() > 1) throw new InvalidOperationException(string.Format("{0} contains more than one record", nameof(filterExpression)));
 
-                entity.Id = entries.First().Id;
                 entity.UtcUpdatedOn = DateTime.UtcNow;
                 entity.UpdatedByUserId = userId;
 
@@ -371,7 +369,6 @@ namespace Luval.DataStore.Extensions
         /// Adds or updates the audit based entity to the <see cref="IDataEntityCollection{TEntity}"/> and saves to the target <see cref="IDataStore"/>
         /// </summary>
         /// <typeparam name="TEntity">The data entity <see cref="Type"/></typeparam>
-        /// <typeparam name="TKey">The entity Id <see cref="Type"/></typeparam>
         /// <param name="uow">Unit of Work to use</param>
         /// <param name="entities">The collection of entities to persist</param>
         /// <param name="filterExpression">Expression used to identify if the record already exists in the <see cref="IDataStore"/></param>
@@ -380,7 +377,7 @@ namespace Luval.DataStore.Extensions
         /// <returns>A <see cref="Task{TResult}"/> with the affected records</returns>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="InvalidOperationException"></exception>
-        public static async Task<int> AddOrUpdateAndSaveAsync<TEntity, TKey>(IUnitOfWork<TEntity> uow, IEnumerable<TEntity> entities, string userId, Expression<Func<TEntity, bool>> filterExpression, CancellationToken cancellationToken) where TEntity : class, IAuditedEntity<TKey>
+        public static async Task<int> AddOrUpdateAndSaveAsync<TEntity>(this IUnitOfWork<TEntity> uow, IEnumerable<TEntity> entities, string userId, Expression<Func<TEntity, bool>> filterExpression, CancellationToken cancellationToken) where TEntity : class, IAuditedEntity
         {
             if (uow == null) throw new ArgumentNullException(nameof(uow));
             if (entities == null) throw new ArgumentNullException(nameof(entities));
@@ -392,7 +389,6 @@ namespace Luval.DataStore.Extensions
                 {
                     if (entries.Count() > 1) throw new InvalidOperationException(string.Format("{0} contains more than one record", nameof(filterExpression)));
 
-                    entity.Id = entries.First().Id;
                     entity.UtcUpdatedOn = DateTime.UtcNow;
                     entity.UpdatedByUserId = userId;
 
